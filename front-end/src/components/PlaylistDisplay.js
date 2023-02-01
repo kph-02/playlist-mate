c3b95bb641fe1700cd96cbe6670f8369809aa72d
kph-02 <karlp.hernande@gmail.com>
2023-02-01 01:36:04 -0800

    Added start of algorithm (cont.)

---
+++ front-end/src/components/PlaylistDisplay.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import '../App.css';
+import { ThemeProvider } from "@mui/material";
+import { TextField }  from "@mui/material";
+import pmLogo from '../assets/Playlist-Mate.png';
+import axios from 'axios';
+
+import { appTheme } from "../themes/themes";
+
+function PlaylistDisplay(props) {
+
+    const [playlistGenerated, setPlaylistGenerated] = useState(false);
+    const { values, handleChange } = props;
+    const accessToken = values.accessToken;
+
+    const [backendData, setBackendData] = useState()
+    
+    useEffect(() => {
+        console.log(values)
+        axios.get('http://localhost:3001/generatePlaylist', {
+            params: values
+        }).then((res) => {
+            console.log(res.data)
+        }).catch(error => {
+            console.log(error);
+        });
+    }, [])
+
+    return (
+        <ThemeProvider theme={appTheme}>
+            {(playlistGenerated == false) &&
+                <img  src={pmLogo} alt="logo"/>
+            }   
+            <br/>
+        </ThemeProvider>
+    );
+}
+
+export default PlaylistDisplay;
